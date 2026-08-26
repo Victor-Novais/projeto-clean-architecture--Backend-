d87dc45504f58d4e24ab3020157e2c5f8cd84a9c
Victor Novais <victornovais1337@gmail.com>
2026-08-26 13:35:02 -0300

    test(room): expandir asserções do caso de uso de criação de quarto

--- src/domain/employee/use-cases/creat-room.spec.ts
+++ src/domain/employee/use-cases/creat-room.spec.ts
@@ -2,6 +2,7 @@ import Room from "../entities/room";
 import Money from "../../shared/value-objects/money";
 import { InMemoryRoomMemory } from "../../../../test/repositories/in-memory-room-repository";
 import { CreatRoomUseCase } from "./create-room";
+import Identity from "../../../core/entities/identity";
 
 let roomRepository: InMemoryRoomMemory;
 let useCase: CreatRoomUseCase;
@@ -18,6 +19,18 @@ describe("Criação de quarto", () => {
     });
 
     expect(room).toBeDefined();
-    expect(roomRepository.items[0]?.name).toEqual("Suite");
+    expect(roomRepository.items[0]!.id).toBeInstanceOf(Identity);
+    expect(roomRepository.items[0]!.id.toString()).toEqual(room.id.toString());
+    expect(roomRepository.items[0]!.name).toEqual("Suite");
+    expect(roomRepository.items[0]!.price.value).toEqual(120000);
+    expect(roomRepository.items[0]!.price.formattedPriceBRL()).toEqual(
+      "R$\u00A01.200,00",
+    );
+    expect(roomRepository.items[0]!.image).toEqual("room.jpg");
+    expect(roomRepository.items[0]!.hasWifi).toBe(false);
+    expect(roomRepository.items[0]!.hasKitchen).toBe(false);
+    expect(roomRepository.items[0]!.hasAir).toBe(false);
+    expect(roomRepository.items[0]!.isAvailable).toBe(true);
+    expect(roomRepository.items[0]!.isPetFriendly).toBe(false);
   });
 });
